06319d2ec3a11f541f0b885f38c7075adb1e40d5
Camilo Andrés Araujo Camelo <caac9530@gmail.com>
2026-02-13 13:48:37 -0500

    actualización proyecto

---
+++ whxdata/toc8.new.js
@@ -0,0 +1,1 @@
+(function() {    var toc =  [{"name":"Seguridad","type":"item","tilePath":"","description":"","url":"Escritos/Seguridad/Seguridad.htm"}];    window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });    })();
